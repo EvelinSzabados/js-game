650970f7e8d87db6faee72f764683d1601f2115e
Evelin Szabados <szabadosevelin7@gmail.com>
2019-11-11 16:06:56 +0100

    Moving spaceship with arrows

--- static/js/game.js
+++ static/js/game.js
@@ -1,8 +1,31 @@
 setInterval(moveBg, 10);
+
 let y = 0;
 function moveBg(){
         let board = document.querySelector('.game_field')
         y--;
         board.style.backgroundPositionY = y + 'px';
 }
+
+const ship = document.getElementById('ship')
+window.addEventListener("keydown", function (event) {
+        let left = ship.style.left === '' ? 10 : parseInt(ship.style.left.replace('px', ''), 10)
+        console.log(left)
+        if (left > 9 && left < 650  ){
+            switch(event.code) {
+                case "ArrowLeft":
+                        ship.style.left = `${left-30}px`;
+
+                        break;
+                case "ArrowRight":
+                        ship.style.left = `${left+30}px`;
+                        break;
+        }
+        }else if (left >= 650){
+                ship.style.left = `${left-20}px`
+        }else{
+                ship.style.left = `${left+20}px`
+        }
+
+})
 
