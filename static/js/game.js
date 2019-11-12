c286deb36d8c1d793d05757ad3c0fddd9b486129
VaradiPeterRoland <varadi.peter.roland95@gmail.com>
2019-11-12 15:34:47 +0100

    made the function that lets the meteor fall down

--- static/js/game.js
+++ static/js/game.js
@@ -1,8 +1,19 @@
 setInterval(moveBg, 10);
 let y = 0;
+let meteorY = 0;
 function moveBg(){
-        let board = document.querySelector('.game_field')
+        let board = document.querySelector('.game_field');
         y--;
         board.style.backgroundPositionY = y + 'px';
 }
 
+let MyVar = setInterval(moveMeteor, 6)
+function moveMeteor(){
+                const meteor = document.getElementById('meteor');
+                meteorY++;
+                if (meteorY >254){
+                        clearInterval(MyVar);
+                }
+                meteor.style.top = 2 * meteorY + 'px';
+
+}
