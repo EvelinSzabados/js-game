fc53d413e5020bb3402120dfdd9aa1e9a0281b83
Evelin Szabados <szabadosevelin7@gmail.com>
2019-11-13 11:05:02 +0100

    Placing bullet

--- static/js/game.js
+++ static/js/game.js
@@ -1,14 +1,13 @@
-setInterval(moveBg, 10);
-let y = 0;
-let meteorY = 0;
 function moveBg(){
+    let y = 0;
         let board = document.querySelector('.game_field');
         y++;
         board.style.backgroundPositionY = y + 'px';
 }
 
-const ship = document.getElementById('ship')
-window.addEventListener("keydown", function (event) {
+function moveShip(){
+    const ship = document.getElementById('ship')
+    window.addEventListener("keydown", function (event) {
         let left = ship.style.left === '' ? 10 : parseInt(ship.style.left.replace('px', ''), 10)
         console.log(left)
         if (left > 10 && left < 1250  ){
@@ -28,14 +27,42 @@ window.addEventListener("keydown", function (event) {
         }
 
 })
-
-let MyVar = setInterval(moveMeteor, 6)
-function moveMeteor(){
-                const meteor = document.getElementById('meteor');
-                meteorY++;
-                if (meteorY >254){
-                        clearInterval(MyVar);
-                }
-                meteor.style.top = 2 * meteorY + 'px';
-
 }
+
+function handleMeteor(){
+    let meteorY = 0;
+    let MyVar = setInterval(moveMeteor, 6)
+        function moveMeteor(){
+
+        const meteor = document.getElementById('meteor');
+        meteorY++;
+        if (meteorY >254){
+            clearInterval(MyVar);
+        }
+        meteor.style.top = 2 * meteorY + 'px';
+
+        }
+}
+
+function handleBullet(){
+    const ship = document.getElementById('ship');
+    const bullet = document.getElementById('bullet');
+    window.addEventListener("keydown", function (event) {
+        let top = bullet.style.left === '' ? 590 : parseInt(bullet.style.left.replace('px', ''), 10);
+        let shipLeft = ship.style.left === '' ? 10 : parseInt(ship.style.left.replace('px', ''), 10)
+        bullet.style.left = `${shipLeft+45}px`
+        if (event.code === "KeyX") {
+            bullet.style.top = `${top - 45}px`;
+        }
+    })
+}
+function main(){
+    setInterval(moveBg, 10);
+    moveShip()
+    handleMeteor()
+    handleBullet()
+}
+
+main()
+
+
